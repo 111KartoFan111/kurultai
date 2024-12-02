36e754a0c531e6686cf9f913ad31f1505f8c5d00
111KartoFan111 <102473619+111KartoFan111@users.noreply.github.com>
2024-12-02 14:22:13 +0500

    Update proekt.sql

--- proekt.sql
+++ proekt.sql
@@ -92,3 +92,46 @@ CREATE TABLE PasswordResetTokens (
 --CREATE INDEX idx_users_email ON Users(email);
 --CREATE INDEX idx_notifications_user_id ON Notifications(user_id);
 --CREATE INDEX idx_matches_tournament_id ON Matches(tournament_id);
+
+
+
+-- Insert 
+INSERT INTO Users (email, phone_number, full_name, gender, date_of_birth, group_num, username, password_hash)
+VALUES 
+('bakosyatt567@gmail.com', '87719815739', 'Тәсібек Бағнұр Таңатарқызы', 'Жен', '1999-01-23', 'ПО2402', 'Dionysius', 'SlavaBobu'),
+('danelya_sm@icloud.com', '+77478480952', 'Смагулова Данеля Меллатовна', 'Жен', '2009-05-02', 'ПО2402', 'danelyasm', 'danelya09'),
+('szarina08@icloud.com', '87786281309', 'Сайдалина Зарина Ерлановна', 'Жен', '2008-11-06', 'ПО-2402', 'Zara', 'Zara2008'),
+('ailisabandhi789@gmail.com', '87761666358', 'Сабанчи Айли Ериккызы', 'Жен', '2008-06-18', 'ПО 2406', 'ailisshka', 'sabanchi'),
+('panchenkoamina@gmail.com', '+77053821335', 'Панченко Амина Андреевна', 'Жен', '2006-04-03', 'ПО2303', 'Aminferrr', 'aminA2025.'),
+('anel94544@gmail.com', '+77775451177', 'Калыкулова Анель Муратовна', 'Жен', '2007-09-04', 'ПО2303', 'Анель', 'Anel'),
+('azhar208asko@gmail.com', '87051032460', 'Зұлхарнай Ажар', 'Жен', '2008-07-10', '2402', 'azhar', '?'),
+('zhumataymahfuza@icloud.com', '87478403626', 'Жұматай Махфуза Мұратқызы', 'Жен', '2008-09-13', 'По 2405', 'Sulybelle', 'mmm555!'),
+('asirbekselin@gmail.com', '+77002709670', 'Әшірбек Селин Әлібекқызы', 'Жен', '2007-08-21', 'ПО-2301', 'mortualin', 'astana2020@'),
+('erkezhanabil@gmail.com', '87789134110', 'Әбіл Еркежан Бағдатқызы', 'Жен', '2009-09-30', 'ПО2402', 'erkezhan.ab', 'Karavana19/'),
+('rmiscuteliololoyo@gmail.com', '+77785749181', 'Адилова Адина Алмазовна', 'Жен', '2007-04-09', 'ПО2306', '9nrj4', 'qwerty12345'),
+('znurlanuly203@gmail.com', '87073804075', 'Тойшыбаев Жаслан', 'Муж', '2008-03-18', 'ПО-2303', 'ENTER', '180308tjN'),
+('sunkaruly@proton.me', '+77718444254', 'Сакенов Ануар Сункарулы', 'Муж', '2008-05-13', 'ПО-2302', 'Anek', 'AnuarSunkarovich_A8'),
+('makazanalpamys@gmail.com', '87058758185', 'Мақажан Алпамыс Мұхамедқалиұлы', 'Муж', '2008-08-11', 'ПО 2309', 'Alpamys', 'Alpamys123'),
+('kystaubainurbakyt@icloud.com', '87758050335', 'Қыстаубай Нұрбахыт Серікұлы', 'Муж', '2008-09-30', '2405', 'nurym', 'abc123@/'),
+('zarkynismagulov@gmail.com', '87762538632', 'Исмагулов Жарқын Маратұлы', 'Муж', '2007-08-27', 'ПО-2301', 'KartoFan', 'pezve4-tydtiB-zonfar'),
+('disaisa01@gmail.com', '87764996998', 'Исин Диас Оразұлы', 'Муж', '2007-07-16', 'ПО-2304', 'Isin_Dias', 'i5india$'),
+('Ermohind@gmail.com', '87000228000', 'Ермохин Данил Дмитриевич', 'Муж', '2008-04-30', 'ПО 2306', 'Benito_big_boy', 'Aska2008'),
+('erahmedkz16@mail.ru', '+77471630707', 'Ермек Ахмед', 'Муж', '2007-07-02', 'ПО2303', 'rxsdf', 'fara#24');
+
+UPDATE Users
+SET role = 'admin'
+WHERE email IN ('bakosyatt567@gmail.com', 'zarkynismagulov@gmail.com', 'asirbekselin@gmail.com', 'sunkaruly@proton.me');
+
+select * from Users 
+
+-- Создаем пользователей
+CREATE ROLE "Dionysius" LOGIN PASSWORD 'SlavaBobu';
+CREATE ROLE "KartoFan" LOGIN PASSWORD 'pezve4-tydtiB-zonfar';
+CREATE ROLE "mortualin" LOGIN PASSWORD 'astana2020@';
+CREATE ROLE "Anek" LOGIN PASSWORD 'AnuarSunkarovich_A8';
+
+-- Назначаем суперпользователями
+ALTER ROLE "Dionysius" WITH SUPERUSER;
+ALTER ROLE "KartoFan" WITH SUPERUSER;
+ALTER ROLE "mortualin" WITH SUPERUSER;
+ALTER ROLE "Anek" WITH SUPERUSER;
